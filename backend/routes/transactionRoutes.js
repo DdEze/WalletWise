d15deb05d3395e78678d6898c504a5cfcae4af86
Ezequiel <ezequielensenada@yahoo.com.ar>
2025-05-26 19:02:19 -0300

    Route Transation

---
+++ backend/routes/transactionRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('../middlewares/auth');
+const { createTransaction, getTransactions, deleteTransaction } = require('../controllers/transactionController');
+
+router.post('/', auth, createTransaction);
+router.get('/', auth, getTransactions);
+router.delete('/:id', auth, deleteTransaction);
+
+module.exports = router;
